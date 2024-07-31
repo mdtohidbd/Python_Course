0b9144f38d6cafe963cb09b47b23e0f2ecad83fc
md tohidul islam <130247094+mdtohidbd@users.noreply.github.com>
2024-07-31 18:21:39 +0600

    Add files via upload

---
+++ Tuples.py
@@ -0,0 +1,23 @@
+#tup = (2,3,1,5)
+#print(type(tup))
+"""
+tup = (1,3,8,3,5)
+print(tup[0])
+print(tup[2])
+
+"""
+
+"""
+tup = ()
+print(tup)
+print(type(tup))
+"""
+"""
+tup = (1,)
+print(tup)
+print(type(tup))
+"""
+tup = (1,2,4,5)
+print(tup[1:3])
+print(tup[:3])
+print(tup[2:])
