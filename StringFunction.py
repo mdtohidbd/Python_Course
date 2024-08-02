243e3a626db3d2d83705e840d61531b81db164e0
md tohidul islam <130247094+mdtohidbd@users.noreply.github.com>
2024-08-02 21:35:15 +0600

    Add files via upload

---
+++ StringFunction.py
@@ -0,0 +1,40 @@
+"""
+str = "I am studying Mymensingh Engineering college"
+print(str.endswith("ege"))
+print(str.endswith("col"))
+"""
+
+"""
+str = "i am studying Mymensingh Engineering college"
+str = str.capitalize()
+print(str)
+"""
+"""
+str = "i am studying Mymensingh Engineering college"
+print(str.replace("o","n"))
+print(str.replace("n","o"))
+"""
+"""
+str = "i am studying Mymensingh Engineering college"
+print(str.find("o"))
+print(str.find("i"))
+print(str.find("M"))
+print(str.find("am"))
+"""
+"""
+str = "i am studying Mymensingh Engineering college"
+print(str.count("m"))
+print(str.find("s"))
+"""
+
+#wap to input user's first name & print its length.
+"""
+name = input("enter your name :")
+print("length of your name is", len(name))
+"""
+
+#wap to find the occurrence of '$' in a String.
+
+
+str = "Hi, $Iam the $ symbol $999,99"
+print(str.count("$")) 
