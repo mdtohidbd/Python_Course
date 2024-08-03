5cc1ad6244779608620a754b37881836daf79ca4
md tohidul islam <130247094+mdtohidbd@users.noreply.github.com>
2024-08-03 19:18:15 +0600

    Add files via upload

---
+++ Slicing.py
@@ -0,0 +1,26 @@
+#str = "Mymensingh"
+#print(str[2:6])
+
+#str = "Mymensingh"
+#print(str[0:8])
+"""
+str = "Tohidul Islam"
+print(str[4:8])
+
+str = "Tohidul Islam"
+print(str[4:10])
+
+str = "Tohidul Islam"
+print(str[4:len(str)])
+
+str = "Tohidul Islam"
+print(str[:4]) #[0:4]
+print(str[7:]) #[7:len(str)]
+"""
+
+
+# Negative index
+
+str = "apple"
+print(str[-3:-1])
+print(str[-5:-2])
