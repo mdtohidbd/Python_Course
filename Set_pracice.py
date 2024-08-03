856bebbd9cec638ded06b7930575368bccd4d065
md tohidul islam <130247094+mdtohidbd@users.noreply.github.com>
2024-08-03 19:19:21 +0600

    Add files via upload

---
+++ Set_pracice.py
@@ -0,0 +1,37 @@
+"""
+dictionary = {
+    "cat" : "a small animal",
+    "tabe" : ["a piece of furniture", "list of facts & figures"]
+
+}
+print(dictionary)
+"""
+"""
+subjects = {
+    "python", "java","C++", "python", "javascript","java",
+    "python", "java","C++","c"
+}
+print(len(subjects))
+
+"""
+"""
+marks = {}
+
+x = int(input("enter phy :"))
+marks.update({"phy" : x})
+
+x = int(input("enter math :"))
+marks.update({"math" : x})
+
+x = int(input("enter chem :"))
+marks.update({"chem" : x})
+
+print(marks)
+"""
+
+values = {
+     ("float",9.0),
+     ("int",9)
+    
+    }
+print(values)
