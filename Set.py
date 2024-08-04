f5f1df23bd53cda393410062146ac45521ea7261
md tohidul islam <130247094+mdtohidbd@users.noreply.github.com>
2024-08-04 12:10:17 +0600

    Add files via upload

---
+++ Set.py
@@ -0,0 +1,11 @@
+"""
+collection = {1,2,3,4,"hello", "world"}
+
+print(collection)
+print(len(collection)) #total num of items
+
+"""
+
+collection = set() #empty set ; syntax
+
+print(type(collection))
