b6426acd8a59afc1671579f2ad9fa4b589d50797
md tohidul islam <130247094+mdtohidbd@users.noreply.github.com>
2024-08-05 23:06:03 +0600

    Add files via upload

---
+++ range.py
@@ -0,0 +1,51 @@
+"""
+seq = range(10)
+
+for i in seq:
+    print(i)
+
+    """
+
+"""
+for i in range(2,10):
+    print(i)
+
+    """
+
+#Evan num:
+"""
+
+for i in range(2,100,2):
+    print(i)
+    """
+
+#odd num:
+
+"""
+
+for i in range(1, 100,2):
+    print(i)
+
+    """
+
+#print numbers from 1 to 100.
+"""
+for i in range(1 ,101):
+    print(i)
+
+    """
+
+
+#print numbers from 100 to 1.
+"""
+
+for i in range(100, 0, -1):
+    print(i)
+
+    """
+
+#print the multiplication tabe of a number n.
+n = int(input("enter number :"))
+
+for i in range(1,11):
+    print(n*i)
