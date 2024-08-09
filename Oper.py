f78dd2173db1bdce062538aa1f394bf1a4edec7b
md tohidul islam <130247094+mdtohidbd@users.noreply.github.com>
2024-08-09 12:31:50 +0600

    Add files via upload

---
+++ Oper.py
@@ -0,0 +1,44 @@
+#arithmetic operators
+"""a = 5
+b = 2 
+
+print(a+b)
+print(a-b)
+print(a*b)
+print(a/b)
+print(a % b) #remainder
+print(a**b) #a^b"""
+
+
+#relational operators
+
+"""a = 50 
+b = 20
+
+print(a == b) #false
+print(a != b) #True
+print(a >= b)#True
+print(a > b)#True
+print(a <= b) #false
+print(a < b) #false """
+
+
+#assignment operators
+"""num = 10
+#num = num + 10 #10+10=>20
+num +=  10
+print("num : ", num)"""
+
+#logical operators
+#type conversion
+"""a = 2
+b = 4.55
+
+sum = a + b  
+print(sum)"""
+
+a =  int("2")
+b = 4.25
+
+print(type(a))
+print(a+b)
