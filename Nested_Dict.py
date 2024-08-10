5d5cdc19f2daf5995b206a97ee27e1d4139e0bc0
md tohidul islam <130247094+mdtohidbd@users.noreply.github.com>
2024-08-10 16:52:45 +0600

    Add files via upload

---
+++ Nested_Dict.py
@@ -0,0 +1,11 @@
+student = {
+    "name" : "Tohidul Islam",
+    "subjects" : {
+        "phy" : 97,
+        "chem" : 98,
+        "math" : 95
+    }
+}
+#nested dictionary
+
+print(student["subjects"]["chem"])
