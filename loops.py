f678f39e263577a4969700005c684e657347d4b5
md tohidul islam <130247094+mdtohidbd@users.noreply.github.com>
2024-08-10 16:53:14 +0600

    Add files via upload

---
+++ loops.py
@@ -0,0 +1,99 @@
+"""
+count = 1
+while count <=5 :
+    print("hello")
+    count += 1
+
+    print(count)
+
+
+    
+i = 1
+while count <=100:
+    print("Tohidul Islam", i)
+    i += 1
+
+"""
+
+#print num from 1 to 5 .
+"""
+i = 1
+while i <= 5:
+    print(i)
+    i +=1
+
+    print("loop ended")
+
+    """
+"""
+i = 5
+while i>=1:
+    print(i)
+    i -=1
+
+    print("loop ended")
+
+    """
+"""
+#print numbers from 1 to 100.
+
+i = 1
+while i<=100 :
+    print(i)
+    i+=1
+
+    print("count")
+    """
+"""
+#print num from 100 to 1.
+
+i = 100
+while i>=1:
+    print(i)
+    i -=1
+
+    print("count")
+
+    """
+
+
+#print the multiplication tabel of a number n.
+"""
+i = 1
+while i<=10:
+    print(3*i)
+    i +=1
+"""
+"""
+n = int (input("enter number :"))
+i = 1
+while i <=10:
+    print(n*i)
+    i += 1
+    """
+
+#print the elements of the following list a loop:
+#[1,4,9,16,25,36,49,64,81,100]
+"""
+nums = [1,4,9,16,25,36,49,64,81,100]
+
+idx = 0
+while idx < len(nums): #nums [0], nums[1],..... 
+    print(nums[idx])
+    idx += 1
+
+    """
+
+#Search for a number x in thix tuple using loop:
+#[1,4,9,16,25,36,49,64,81,100]
+
+nums = (1,4,9,16,25,36,49,64,81,100)
+
+x = 36
+i = 0
+while i<len(nums):
+    if(nums[i] == x):
+        print("Found at idx", i)
+    i += 1
+    
+    
