b3507a03e5bb42fa7e1f1e4099bd788be7be6bef
md tohidul islam <130247094+mdtohidbd@users.noreply.github.com>
2024-08-10 16:53:53 +0600

    Add files via upload

---
+++ Lists_Methods.py
@@ -0,0 +1,46 @@
+"""
+list = [2,1,3,4,5]
+list.append(6)
+print(list)
+"""
+"""
+list = [2,6,1,7,3]
+print(list.append(4))
+print(list.sort())
+print(list) #ascending
+
+"""
+"""
+list = [2,6,1,7,3]
+print(list.append(4))
+print(list.sort(reverse=True))#deascending
+print(list)
+"""
+"""
+list = ['a','v','w','t','j','k','b','c']
+#print(list.append(4))
+print(list.sort())
+print(list)
+
+"""
+"""
+list = ['a','v','w','t','j','k','b','c']
+#print(list.append(4))
+print(list.sort(reverse=True))
+print(list)
+"""
+"""
+list = [9,4,3,2,5,8]
+list.reverse()
+print(list)
+
+"""
+"""
+list = [2,1,3]
+list.insert(1,5)
+print(list)
+"""
+
+list = [2,1,3]
+list.pop(1)
+print(list)
