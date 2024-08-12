c45eec251219820b3cda165271e39f3698db0531
md tohidul islam <130247094+mdtohidbd@users.noreply.github.com>
2024-08-12 10:57:50 +0600

    Add files via upload

---
+++ Indexing.py
@@ -0,0 +1,28 @@
+#str = "Engineering college"
+#ch = str[3]
+#print(ch)
+
+
+#str = "Engineering college"
+#ch = str[9]
+#print(ch)
+
+#str = "Engineering college"
+#ch = str[0]
+#print(ch)
+
+
+str = "Engineering college"
+ch = str[12]
+print(ch)
+
+
+
+str = "Engineering college"
+ch = str[5]
+print(ch)
+
+
+str = "Engineering college"
+ch = str[15]
+print(ch)
