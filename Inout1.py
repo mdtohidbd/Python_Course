64e7a4940b070ae5c6326cd31d8b3d95943695b2
md tohidul islam <130247094+mdtohidbd@users.noreply.github.com>
2024-08-12 10:57:25 +0600

    Add files via upload

---
+++ Inout1.py
@@ -0,0 +1,29 @@
+#write a program to input 2 numbers & print their sum
+
+"""first = int (input("enter first :"))
+second = int (input("enter second :"))
+
+print("sum=", first + second)"""
+
+
+
+#WAP to input side of a square & print its area
+
+"""side = float(input("enter square side :"))
+print("area =", side * side) """
+
+#WAP to input 2 floating point  numbers & print their average
+
+"""a= float(input("enter first :"))
+b= float(input("enter second  :"))
+print("avg =",(a+b)/2)"""
+
+
+#wap to input 2 int numbers a and b.
+#print True if a is greater than or equal to b . if not print False.
+
+a= int(input("enter first :"))
+b= int(input("enter second  :"))
+
+print(a >= b)
+
