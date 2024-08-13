da9c67f9a28418089ff706255c354478a7df0871
md tohidul islam <130247094+mdtohidbd@users.noreply.github.com>
2024-08-13 22:37:04 +0600

    Add files via upload

---
+++ Grade.py
@@ -0,0 +1,26 @@
+"""
+marks = 74 
+
+if(marks >= 90):
+    grade = "A"
+elif(marks >= 80 and marks <90):
+    grade = "B"
+elif(marks >= 70 and marks <80):
+    grade = "C"
+else:
+    grade = "D"
+
+print("grade of the student ->",grade)
+"""
+marks = int (input("enter student marks :"))
+
+if(marks >= 90):
+    grade = "A"
+elif(marks >= 80 and marks <90):
+    grade = "B"
+elif(marks >= 70 and marks <80):
+    grade = "C"
+else:
+    grade = "D"
+
+print("grade of the student ->",grade)
