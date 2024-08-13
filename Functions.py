cce19df59e04d42f504a202c1d7b6ec70fa43348
md tohidul islam <130247094+mdtohidbd@users.noreply.github.com>
2024-08-13 22:38:32 +0600

    Add files via upload

---
+++ Functions.py
@@ -0,0 +1,55 @@
+"""
+def calc_sum(a , b):
+    sum = a + b
+    print(sum)
+    return sum
+
+
+calc_sum(5 ,10 )
+calc_sum(2,10 )
+calc_sum(7 ,10 ) 
+"""
+"""
+#function definition
+def calc_sum(a , b): #parameters
+    return a+b
+
+sum = calc_sum(1,2)#function call ; arguments
+print(sum)
+
+
+"""
+
+#average of 3 numbers
+"""
+def calc_avg(a, b, c):
+    sum = a + b + c
+    avg = sum/3
+    print(avg)
+    return avg
+
+calc_avg(1,2,3)
+
+"""
+"""
+def calc_avg(a, b, c):
+    sum = a + b + c
+    avg = sum/3
+    print(avg)
+    return avg
+
+calc_avg(98,97,95)
+
+
+"""
+"""
+def calc_avg(a, b, c):
+    sum = a + b + c
+    avg = sum/3
+    print(avg)
+    return avg
+
+calc_avg(98,97,100)
+
+"""
+
