6a84a8577c002ac91c7a78170385c347ce4eb301
md tohidul islam <130247094+mdtohidbd@users.noreply.github.com>
2024-08-14 23:21:39 +0600

    Add files via upload

---
+++ For_loop.py
@@ -0,0 +1,34 @@
+"""
+nums = [1,2,3,4,5,]
+
+for val in nums:
+    print(val)
+    """
+
+"""
+
+tup = (3,4,5,6,7)
+
+for num in tup:
+    print(num)
+
+    """
+"""
+str = "Tohidul"
+
+for char in str:
+    print(char)
+
+else:
+    print("End")
+
+    """
+
+str = "college"
+
+for char in str:
+    if(char == 'o'):
+        print("o found")
+        break
+    print(char)
+print("END")
