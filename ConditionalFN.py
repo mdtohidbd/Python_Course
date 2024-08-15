f1307ad2f5e8338ba67706d849e8e6bee8db3b7c
md tohidul islam <130247094+mdtohidbd@users.noreply.github.com>
2024-08-15 21:50:53 +0600

    Add files via upload

---
+++ ConditionalFN.py
@@ -0,0 +1,34 @@
+"""
+light = "green"
+
+if(light == "red"):
+    print("stop")
+elif(light == "green"):
+    print("Go")
+elif(light == "yellow"):
+    print("look")
+else:
+    print("light is broken")
+
+print("end of code")
+"""
+"""
+light = "pink"
+
+if(light == "red"):
+    print("stop")
+elif(light == "green"):
+    print("Go")
+elif(light == "yellow"):
+    print("look")
+else:
+    print("light is broken")
+
+print("end of code")
+"""
+
+age = 24
+if(age >=18):
+    print("can vote")
+else:
+    print("CANNOT VOTE")
