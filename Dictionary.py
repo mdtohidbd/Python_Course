ceacb55855ba920622a49899cbdfaa6d01a7e355
md tohidul islam <130247094+mdtohidbd@users.noreply.github.com>
2024-08-15 21:50:16 +0600

    Add files via upload

---
+++ Dictionary.py
@@ -0,0 +1,46 @@
+"""
+info = {
+    "key" : "value",
+    "name" : "college",
+    "learning" : "coding" ,
+    "age" : 35,
+    "is_adult" : True,
+    "marks" : 94.4
+
+}
+print(info )
+
+"""
+"""
+
+info = {
+    "name" : "college",
+    "subjects" : ["python", "c", "java"],
+    "topics" : ("dict", "set"),
+    "age" : 35,
+    "is_adult" : True,
+    12.99: 94.4
+
+}
+print(info["name"])
+print(info["topics"])
+print(info["age"])
+
+"""
+
+
+info = {
+    "name" : "college",
+    "subjects" : ["python", "c", "java"],
+    "topics" : ("dict", "set"),
+    "age" : 35,
+    "is_adult" : True,
+    12.99: 94.4
+
+}
+
+info["name"] = "Tohdul"
+info["surname"] = "Islam"
+print(info)
+
+
