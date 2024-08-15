64d333c15ab2e170e45c4e5dace42b6848392b35
md tohidul islam <130247094+mdtohidbd@users.noreply.github.com>
2024-08-15 21:51:15 +0600

    Add files via upload

---
+++ Conditional.py
@@ -0,0 +1,11 @@
+light = input("light color :")
+
+if(light == "red"):
+    print("stop")
+
+elif(light == "yellow"):
+    print("look")
+elif(light == "green"):
+    print("go")
+else:
+    print("light is broken")
